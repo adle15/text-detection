80fa6d12041384a648f4de60003c7340bd7644f8
adle15 <adli.poang@gmail.com>
2020-07-22 21:13:30 +0700

    Adding For External Webcam and successfuly link to UI

--- mainwindow(extern_cam).py
+++ mainwindow(extern_cam).py
@@ -8,6 +8,10 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 from PyQt5.uic import loadUi
+import pytesseract
+from PIL import ImageGrab
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 
 class tehseencode(QDialog):
@@ -37,11 +41,21 @@ class tehseencode(QDialog):
         # print(cap.read())
         while (cap.isOpened()):
             ret, frame = cap.read()
-
+            timer = cv2.getTickCount()
             if ret == True:
+                timer = cv2.getTickCount()
                 print('here')
                 self.displayImage(frame, 1)
-                cv2.waitKey()
+                hImg, wImg,_ = frame.shape
+                boxes = pytesseract.image_to_boxes(frame)
+                for b in boxes.splitlines():
+                    #print(b)
+                    b = b.split(' ')
+                    #print(b)
+                    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+                    cv2.rectangle(frame, (x,hImg- y), (w,hImg- h), (50, 50, 255), 2)
+                    cv2.putText(frame,b[0],(x,hImg- y+25),cv2.FONT_HERSHEY_SIMPLEX,1,(50,50,255),2)
+                    cv2.waitKey()
                 if (self.logic == 2):
                     self.value = self.value + 1
                     cv2.imwrite('E:/Kuliah/KP/text-detection/%s.png' % (self.value), frame)
